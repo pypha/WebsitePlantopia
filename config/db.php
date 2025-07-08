89fce78515d257e8d29b96407650702aba48810c
pypha <afifa.azzahra-2023@fst.unair.ac.id>
2025-07-08 07:04:55 +0700

    db.php

--- config/db.php
+++ config/db.php
@@ -1,8 +1,8 @@
 <?php
-$host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'plantopia';
+$host = 'sql302.infinityfree.com';
+$user = 'if0_39349773';
+$password = 'vRZqGdsj3mD3ih';
+$database = 'if0_39349773_plantopia';
 
 $conn = mysqli_connect($host, $user, $password, $database);
 if (!$conn) {
